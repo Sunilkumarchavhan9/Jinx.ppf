1f672dba746aa6f0c850636d1a25894244102b49
Vegito <chavhansunilkumar753@gmail.com>
2026-02-16 18:08:13 +0530

    added  new avatar

--- src/features/portfolio/data/user.ts
+++ src/features/portfolio/data/user.ts
@@ -30,10 +30,10 @@ export const USER: User = {
   about: `
 I’m Sunil Kumar Chavhan — a Full-Stack & Blockchain Polymath who loves building systems that scale, ship fast, and break limits. I work across the entire stack: TypeScript, Next.js, Node.js, and cloud-native infrastructure, while also crafting secure on-chain solutions using Solana, Rust, and Solidity.
 
-I've been a 5x Hackathon Winner and Top 4 Solana Hackathon Finalist, selected as one of only 72 fellows from 3,000 applicants, and part of the GitHub Campus Expert Summer 2025 cohort. I thrive at the intersection of Web2 × Web3—where real-time systems, microservices, and decentralized architectures meet.
+I have been a 5x Hackathon Winner and Top 4 Solana Hackathon Finalist, selected as one of only 72 fellows from 3,000 applicants, and part of the GitHub Campus Expert Summer 2025 cohort. I thrive at the intersection of Web2 × Web3—where real-time systems, microservices, and decentralized architectures meet.
 `,
-  avatar: "/jinx.jpg",
-  ogImage: "/jinx.jpg",
+  avatar: "/jinx1.jpeg",
+  ogImage: "/jinx1.jpeg",
   timeZone: "Asia/Kolkata",
   keywords: [
     "sunil kumar chavhan",
